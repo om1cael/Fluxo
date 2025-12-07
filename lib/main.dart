e077dcc04bcf2a5eb1a91be67091c08e042c94d3
om1cael <188113439+om1cael@users.noreply.github.com>
2025-12-07 13:29:39 -0300

    feat: load dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:fluxo/ui/home/view/home_screen.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load();
   runApp(const Fluxo());
 }
 
