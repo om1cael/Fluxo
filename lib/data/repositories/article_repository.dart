055ff941913c24ef3e0e1406984b711f002e489b
om1cael <188113439+om1cael@users.noreply.github.com>
2025-12-08 19:14:35 -0300

    feat: create providers for api_service and article_repository

--- lib/data/repositories/article_repository.dart
+++ lib/data/repositories/article_repository.dart
@@ -1,5 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluxo/data/services/api_service.dart';
 import 'package:fluxo/domain/models/article.dart';
+
+final articleRepositoryProvider = Provider<ArticleRepository>((ref) {
+  final apiService = ref.read(apiServiceProvider);
+  return ArticleRepository(apiService: apiService);
+});
 
 class ArticleRepository {
   ArticleRepository({
